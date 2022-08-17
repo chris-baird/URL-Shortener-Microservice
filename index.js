ab023d698ae7d15f147fe701e1ddd8108a052ece
Chris Baird <cab3953@gmail.com>
2022-08-16 21:26:17 -0700

    Configured middleware

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 3000;
 
 const urlMap = new Map()
 app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
+app.use(express.urlencoded({ extended: true }))
 app.use(cors());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
